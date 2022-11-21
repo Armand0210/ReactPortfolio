d95caa42e50fc4566516857efdcc55998be15c24
Armand Aggenbag <aggenbag.a@gmail.com>
2022-11-21 16:41:33 +0200

    FolderStructure

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/About';
+import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
   );
 }
-
-export default App;
